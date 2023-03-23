adccc586d7b9eb08d49a90b82ed3f2ad35daf1ca
Yuran Simao <yuransimao7@gmail.com>
2023-03-23 12:46:35 +0100

    adicionando estilo no componente CardR

--- src/Components/Layout/Card/CardR/CardR.jsx
+++ src/Components/Layout/Card/CardR/CardR.jsx
@@ -5,11 +5,11 @@ export function CardR (props){
     return(
     <div className={Styles.Card_conteiner}>
         <div className={Styles.Card_text}>
-            <h1>Iphone</h1>
+            <h1>Iphone 11 pro max</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
         </div>
         <div className={Styles.Card_img}>
-            <img src={iphone} alt={iphone} />
+            <img src={iphone} alt={iphone} width="280" />
         </div>
 
         <div className={Styles.Card_button}>
