61707f1c5dd0f39c6d9a65b5b8270c110eec1c1a
Yuran Simao <yuransimao7@gmail.com>
2023-03-24 12:10:37 +0100

    adicionando link ao subMenu

--- src/Components/Layout/SubMenu/submenu.jsx
+++ src/Components/Layout/SubMenu/submenu.jsx
@@ -1,8 +1,13 @@
+import {NavLink} from "../Navbar/NavLink/Navlink"
 import Styles from "./Styles.module.scss"
 export function SubMenu(){
     return(
         <div className={Styles.Submenu}>
-            <div className={Styles.Submenu_items}></div>
+            <div className={Styles.Submenu_items}>
+                <nav>
+                <NavLink Class='Submenu_Link'/>
+                </nav>
+            </div>
         </div>
     )
 }
