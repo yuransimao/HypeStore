acdadd6742a21b56c82c6be3f34ae642f8381b1f
Yuran Simao <yuransimao7@gmail.com>
2023-03-24 15:40:48 +0100

    update

--- src/Components/Layout/Slide/Swiper.jsx
+++ src/Components/Layout/Slide/Swiper.jsx
@@ -6,11 +6,12 @@ import { CardR } from "../Card/CardR/CardR";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 import Styles from"./styles.module.scss";
 
 // import required modules
-import { Autoplay, Pagination, Mousewheel } from "swiper";
+import { Autoplay, Pagination, Mousewheel, Navigation } from "swiper";
 
 
 
@@ -22,16 +23,13 @@ export  default function  Swippers() {
   return (
     <>
     <Swiper
-      direction={"vertical"}
+      
       autoplay={{
         delay: 2500,
   
       }}
-      pagination={{
-        clickable: true,
-      }}
-      mousewheel={true}
-      modules={[Autoplay, Pagination,  Mousewheel]}
+      navigation={true}
+      modules={[Autoplay,  Navigation]}
       className={Styles['mySwiper']}
     >
       <SwiperSlide className={Styles['swiper-slide']}><CardR/></SwiperSlide>
