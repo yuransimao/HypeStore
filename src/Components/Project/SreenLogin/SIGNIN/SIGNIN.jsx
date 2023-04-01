b1a85b7b675eb7e4912caa2a69c9f16738b11915
Yuran Simao <yuransimao7@gmail.com>
2023-04-01 20:21:06 +0100

    validando formulaio Sign in

--- src/Components/Project/SreenLogin/SIGNIN/SIGNIN.jsx
+++ src/Components/Project/SreenLogin/SIGNIN/SIGNIN.jsx
@@ -5,23 +5,54 @@ import {FormButton} from "../../../Layout/Form/FormButton/FormButton"
 import {BsEnvelope} from "react-icons/bs"
 import {BiLockAlt} from "react-icons/bi"
 import {useState} from 'react'
+import { signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from '../../../../../Backend/Service/firbase'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {Loader} from "../../../Loader/loader"
+import {useNavigate} from "react-router-dom"
 
 
 export function SIGNIN ({funcNav}){
     funcNav ( false )
 
-    const [name, SetName] =useState('')
+   
     const [email, SetEmail] =useState('')
     const [password, SetPassword] =useState('')
+    const [isLoading, SetIsLoading] =useState(false)
+    const navigation = useNavigate()
+
+    const loginUser = (e) =>{
+        e.preventDefault()
+        SetIsLoading(true)
+
+        signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+   
+    const user = userCredential.user;
+    SetIsLoading(false)
+    navigation('/')
+    toast.success('Login Sucess')
+    
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    toast.error(error.message)
+    SetIsLoading(false)
+  });
+    }
  return(
     <>
+
+    <ToastContainer/>
+    {isLoading && <Loader/>}
     <Login
     text1="Sign in with:"
     text2="Are you already registered ?"
     Span="Sign up"
     img={Teamamico}
-    value={name}
-    setInput={SetName}
+    SubmitBtn={loginUser}
     input2={
     <Input
     text="Email"
