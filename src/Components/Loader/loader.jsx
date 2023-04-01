4c1bcb6eb8a7b4d18dd5021d68ce5a965127e657
Yuran Simao <yuransimao7@gmail.com>
2023-04-01 14:48:01 +0100

    update

--- src/Components/Loader/loader.jsx
+++ src/Components/Loader/loader.jsx
@@ -1,4 +1,14 @@
 import Styles from "./styles.module.scss"
-export function Loader(){
-
+import Storet from "../../assets/StoreT.gif"
+import ReactDOM from 'react-dom'
+import React from "react"
+export const Loader =()=>{
+    return ReactDOM.createPortal(
+    <div className={Styles.wrapper}>
+        <div className={Styles.loader}>
+            <img src={Storet} alt='loading...'/>
+        </div>
+    </div>,
+    document.getElementById('loader')
+    )
 }
