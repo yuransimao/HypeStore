a7888b22ad51c3150a15d0fbe1b737f4bc022aad
Yuran Simao <yuransimao7@gmail.com>
2023-04-03 16:39:05 +0100

    update

--- src/Components/Layout/User/user.jsx
+++ src/Components/Layout/User/user.jsx
@@ -1,7 +1,7 @@
 import Styles from"./styles.module.scss"
-import { Link } from "react-router-dom"
-import {ImCross} from "react-icons/im"
 
+import {ImCross} from "react-icons/im"
+import { LinkButton } from "../Button/button"
 export function UserMenu({setUserMenu}){
     
 
@@ -9,17 +9,24 @@ export function UserMenu({setUserMenu}){
     return(
 
         <div className={Styles.User}>
-
-                        <div className={}>
-                            <div>
-                            <Link to='/SIGNUP'>Sign up</Link>
-                                                </div>
-                                                <div>
-                            <Link to='/SIGNIN'>Sign In</Link>
-                                                </div>
-                        </div>
-
             <button className={Styles.User_btnCross} onClick={() => setUserMenu(false)}><ImCross/></button>
+        <div className={Styles.Userlink}>
+           
+
+                <div className={Styles.LinkButton}>
+            <LinkButton to='/SIGNIN'
+            txt={'Sign In'}
+            ClassBtn={'BotN1'}
+            />
+            </div>
+            <div className={Styles.LinkButton}>
+            <LinkButton ClassBtn={'BotN2'}to='/SIGNUP'
+            txt='Sign up'
+            />
+            </div>
+        </div>
+
+           
        
         </div>
     )
