e226aa9d9f6cd048862af24ebd56c59f69426d6f
Yuran Simao <yuransimao7@gmail.com>
2023-04-04 14:04:21 +0100

    update

---
+++ src/Redux/Slice/Authslice.jsx
@@ -0,0 +1,29 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+    isLoggedIn: false,
+    email: null,
+    userName: null,
+    userID: null,
+    userPHOTO:null
+}
+
+const authSlice = createSlice({
+    name:'auth',
+    initialState,
+    reducer: {
+        SET_ACTIVE_USER :(state, action) =>{
+            console.log(action.payload)
+        }
+    }
+})
+
+export const  {SET_ACTIVE_USER} = authSlice.actions
+
+export const selectIsloggedIn = (state) => state.auth.isLoggedIn
+export const selectEmail = (state) => state.auth.email
+export const selectUserName = (state) => state.auth.username
+export const selectUserID = (state) => state.auth.userID
+export const selectUserPhoto = (state) => state.auth.userPHOTO
+
+export default authSlice.reducer
