6d71b4a2afdc41f56d23ac8041d0bee57740e417
Yuran Simao <yuransimao7@gmail.com>
2023-04-06 20:37:24 +0100

    adicionando option

--- src/Components/Pages/Loja/Loja.jsx
+++ src/Components/Pages/Loja/Loja.jsx
@@ -23,6 +23,21 @@ export function Loja(){
         <div className={Styles.Icons}>{Arrow} <span>1-year minimum warranty</span></div>
         
    </div>
+
+   <div className={Styles.Loja_wapper}>
+
+       <div className={Styles.wapper_text}>
+           <h1>Refurbished Smartphones</h1>
+           <p>Buy refurbished phones for cheap when you shop at Back Market. Explore our range of refurbished android phones and iPhones and grab yourself the latest and cheapest smartphone only at Back Market. Help prevent e-waste and buy refurbished phones instead.</p>
+       </div>
+       <div className={Styles.wapper_options}> 
+        <select>
+            <option>Best seletter</option>
+            <option>Price : Low to High</option>
+            <option>Price : High to Low</option>
+        </select>
+       </div>
+   </div>
    </div>
 </div>
 
