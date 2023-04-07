cb05234c774094cf5e8a03c6e90b20c9cd14448d
Yuran Simao <yuransimao7@gmail.com>
2023-04-07 19:58:44 +0100

    update

--- src/Components/Pages/Loja/Categorys/categorys.jsx
+++ src/Components/Pages/Loja/Categorys/categorys.jsx
@@ -9,19 +9,19 @@ export function Categorys(){
         <ul className={Styles.Categoria_Lista}>
             <li><NavLink>Produtos de alta tecnologia</NavLink></li>
             <li className={Styles.subItem}>
-            <NavLink className={(isActive) => console.log(isActive)}>smartphones</NavLink>
+            <NavLink className={({isActive}) => isActive ? `${Styles.Active}` : ""}>smartphones</NavLink>
             <ul className={Styles.subItem_category}>
-                <li><NavLink>Iphone</NavLink></li>
-                <li><NavLink>acessórios para smartphones</NavLink></li>
-                <li><NavLink>Smartphones 5G</NavLink></li>
-                <li><NavLink>Smartphones com chip duplo</NavLink></li>
+                <li className={Styles.items}><NavLink>Iphone</NavLink></li>
+                <li className={Styles.items}><NavLink>acessórios para smartphones</NavLink></li>
+                <li className={Styles.items}><NavLink>Smartphones 5G</NavLink></li>
+                <li className={Styles.items}><NavLink>Smartphones com chip duplo</NavLink></li>
                 {visivel === true ?(
                     <>
-                    <li><NavLink>Smartphones Android</NavLink></li>
-                    <li><NavLink>Bons negócios</NavLink></li>
-                    <li><NavLink>Smartphones dobráveis</NavLink></li>
-                    <li><NavLink>iPhones com baterias novas</NavLink></li>
-                    <li><NavLink>Smartphones antigos</NavLink></li>
+                    <li className={Styles.items}><NavLink>Smartphones Android</NavLink></li>
+                    <li className={Styles.items}><NavLink>Bons negócios</NavLink></li>
+                    <li className={Styles.items}><NavLink>Smartphones dobráveis</NavLink></li>
+                    <li className={Styles.items}><NavLink>iPhones com baterias novas</NavLink></li>
+                    <li className={Styles.items}><NavLink>Smartphones antigos</NavLink></li>
                     </> ): ""
                 }
                 <li><button onClick={() => setVisivel(!visivel)}>{visivel ? "Ver menos" : "Ver mais"}</button></li>
