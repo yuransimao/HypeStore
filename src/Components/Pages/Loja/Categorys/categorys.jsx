b8f60cf3b2d7467ad2ffa28a97569d56928ca1c4
Yuran Simao <yuransimao7@gmail.com>
2023-04-07 18:02:24 +0100

    estilizando Loja Categoria

--- src/Components/Pages/Loja/Categorys/categorys.jsx
+++ src/Components/Pages/Loja/Categorys/categorys.jsx
@@ -1,25 +1,32 @@
 import { NavLink } from "react-router-dom"
 import Styles from "./styles.module.scss"
+import {useState} from "react"
 export function Categorys(){
+    const [visivel, setVisivel] = useState(false)
     return(
         <div className={Styles.Categorias}>
-        <h3>Categorias</h3>
+        <h2>Categorias</h2>
         <ul className={Styles.Categoria_Lista}>
             <li><NavLink>Produtos de alta tecnologia</NavLink></li>
-            <li>
-            <NavLink>smartphones</NavLink>
-            <ul>
+            <li className={Styles.subItem}>
+            <NavLink className={(isActive) => console.log(isActive)}>smartphones</NavLink>
+            <ul className={Styles.subItem_category}>
                 <li><NavLink>Iphone</NavLink></li>
                 <li><NavLink>acessórios para smartphones</NavLink></li>
                 <li><NavLink>Smartphones 5G</NavLink></li>
                 <li><NavLink>Smartphones com chip duplo</NavLink></li>
-                <li><NavLink>Smartphones Android</NavLink></li>
-                <li><NavLink>Bons negócios</NavLink></li>
-                <li><NavLink>Smartphones dobráveis</NavLink></li>
-                <li><NavLink>iPhones com baterias novas</NavLink></li>
-                <li><NavLink>Smartphones antigos</NavLink></li>
+                {visivel === true ?(
+                    <>
+                    <li><NavLink>Smartphones Android</NavLink></li>
+                    <li><NavLink>Bons negócios</NavLink></li>
+                    <li><NavLink>Smartphones dobráveis</NavLink></li>
+                    <li><NavLink>iPhones com baterias novas</NavLink></li>
+                    <li><NavLink>Smartphones antigos</NavLink></li>
+                    </> ): ""
+                }
+                <li><button onClick={() => setVisivel(!visivel)}>{visivel ? "Ver menos" : "Ver mais"}</button></li>
             </ul>
-            <li><button>Ver mais</button></li>
+            
             </li>
         </ul>
         </div>
